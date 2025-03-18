80362ebc1c574e791ec679a943910fff10db9e6b
SonYoungJun <dudwnsths1@naver.com>
2025-03-18 20:40:33 +0900

    FEAT:  프로젝트 홈 라우팅 구조 수정 및 추가
    - 프로젝트 홈 라우팅 구조 추가
    - 기본 경로 설정으로 초기화면이 작업 보드로 리디렉션됨

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,9 +1,12 @@
-import { Route } from "react-router-dom";
+import { Navigate, Route } from "react-router-dom";
 import App from "../App";
 import Main from "../pages/wireframe-main/Main";
 import Project from "../pages/wireframe-project/Project";
 import VideoRoom from "../pages/wireframe-videoroom/VideoRoom";
 import RootLayout from "../layout/root-layout";
+import Settings from "../pages/wireframe-project/wireframe-settings/Settings";
+import WorkBoard from "../pages/wireframe-project/wireframe-workboard/WorkBoard";
+import Document from "../pages/wireframe-project/wireframe-document/Document";
 
 const Router = [
   {
@@ -11,7 +14,16 @@ const Router = [
     element: <RootLayout />,
     children: [
       { index: true, element: <Main /> },
-      { path: "project", element: <Project /> },
+      {
+        path: "project",
+        element: <Project />,
+        children: [
+          { index: true, element: <Navigate to="workboard" /> },
+          { path: "workboard", element: <WorkBoard /> },
+          { path: "document", element: <Document /> },
+          { path: "settings", element: <Settings /> },
+        ],
+      },
       { path: "video", element: <VideoRoom /> },
     ],
   },
