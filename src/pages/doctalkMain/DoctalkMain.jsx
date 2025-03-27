6322a482c702576f287d5bd263c9739874b892c3
jaeyoungmun <mjy0694@naver.com>
2025-03-27 23:10:13 +0900

    FEAT: 메인컨테이너 레이아웃 설정

--- src/pages/doctalkMain/DoctalkMain.jsx
+++ src/pages/doctalkMain/DoctalkMain.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import * as S from "./DoctalkMain.styled";
-import { NavBar } from "../wireframe-project/Project.styled";
 import Navbar from "../../components/Navbar/Navbar";
+import Sidebar from "../../components/FeatureWireframeMain/Sidebar/Sidebar";
 
 const DoctalkMain = () => {
   return (
     <S.Container>
       <Navbar />
+      {/* <S.SidebarOverlay>
+        <Sidebar />
+        와이어프레임 사이드바라서 지워야함
+      </S.SidebarOverlay> */}
       <S.MainContentContainer>
         <S.WelcomeCommentText>
           독톡님, 오늘도 힘차게 시작해볼까요!
         </S.WelcomeCommentText>
+        <S.BodyContainer>
+          <S.ScheduleContainer>
+            <S.PreviewTitleText>예정된 이벤트</S.PreviewTitleText>
+            <S.ScheduleBoard>{/* 스케줄리스트 컴포넌트 */}</S.ScheduleBoard>
+          </S.ScheduleContainer>
+          <S.ProjectContainer>
+            <S.PreviewTitleText>나의 프로젝트</S.PreviewTitleText>
+            <S.ProjectBoard>{/* 프로젝트카드 컴포넌트 */}</S.ProjectBoard>
+          </S.ProjectContainer>
+        </S.BodyContainer>
       </S.MainContentContainer>
     </S.Container>
   );
--- src/pages/doctalkMain/DoctalkMain.styled.js
+++ src/pages/doctalkMain/DoctalkMain.styled.js
@@ -9,6 +9,15 @@ const Container = styled.div`
   background: var(--gray-50, #f9fafb);
 `;
 
+const SidebarOverlay = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  z-index: 10;
+`;
+
 const MainContentContainer = styled.div`
   display: flex;
   width: 1280px;
@@ -38,4 +47,72 @@ const WelcomeCommentText = styled.p`
   line-height: 140%; /* 42px */
   letter-spacing: -1.2px;
 `;
-export { Container, MainContentContainer, WelcomeComment, WelcomeCommentText };
+
+const BodyContainer = styled.div`
+  display: flex;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  align-self: stretch;
+`;
+const ScheduleContainer = styled.div`
+  display: flex;
+  width: 628px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 12px;
+`;
+const ScheduleBoard = styled.div`
+  display: flex;
+  padding: 30px 20px 20px 20px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 60px;
+  align-self: stretch;
+  border-radius: 12px;
+  border: 1px solid var(--gray-200, #e5e7eb);
+  background: var(--0, #fff);
+`;
+
+const ProjectContainer = styled.div`
+  display: flex;
+  width: 628px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 12px;
+`;
+const ProjectBoard = styled.div`
+  //grid로 바꾸기
+  display: flex;
+  align-items: flex-start;
+  align-content: flex-start;
+  gap: 24px;
+  align-self: stretch;
+  flex-wrap: wrap;
+`;
+const PreviewTitleText = styled.p`
+  align-self: stretch;
+  color: var(--gray-400, #9ca3af);
+  /* sm Semi Bold */
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 140%; /* 19.6px */
+  letter-spacing: -0.14px;
+`;
+
+export {
+  Container,
+  MainContentContainer,
+  WelcomeComment,
+  WelcomeCommentText,
+  SidebarOverlay,
+  BodyContainer,
+  ScheduleContainer,
+  ProjectContainer,
+  PreviewTitleText,
+  ScheduleBoard,
+  ProjectBoard,
+};
