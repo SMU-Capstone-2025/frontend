ce63c4faff6f49293eb3c08319a95a3ae13e082c
SonYoungJun <dudwnsths1@naver.com>
2025-04-07 01:27:42 +0900

    refactor(document): 문서 리스트 관련 TailwindCSS로 변경
    - DocumentControl, List styled-components -> tailwind 스타일로 전환

---
+++ src/components/document-element/common/DocumentControl.jsx
@@ -0,0 +1,19 @@
+import { SearchIcon } from "../../../assets/icons/index";
+
+const DocumentControl = ({ value, onChange }) => {
+  return (
+    <div className="relative w-full max-w-[471px]">
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        className="flex w-full h-10 px-4 py-2 justify-end items-center gap-[10px] rounded-full border border-[#E8E8E8] bg-[#F5F5F5]"
+      />
+      <div className="absolute top-1/2 right-3 -translate-y-1/2 text-gray-400 pointer-events-none">
+        <SearchIcon />
+      </div>
+    </div>
+  );
+};
+
+export default DocumentControl;
