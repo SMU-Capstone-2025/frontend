14b9fd08c0d7295eeb8a0e3312c366e242d2fa1b
jaeyoungmun <mjy0694@naver.com>
2025-04-09 16:34:34 +0900

    FEAT: 로그인 회원가입 프로젝트생성시에 사용하는 인풋창 컴포넌트로 분리

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Input = ({ type, title, placeholder, value, onchange }) => {
+  return (
+    console.log({ onchange }),
+    (
+      <div className="w-full h-full flex flex-col justify-start items-start gap-1">
+        <div className="justify-start text-gray-800 text-base font-semibold font-['Pretendard']">
+          {title}
+        </div>
+        <input
+          className="w-full h-12 py-3 px-4 rounded-lg outline outline-1 outline-gray-300 overflow-hidden"
+          type={type}
+          value={value}
+          onChange={onchange}
+          placeholder={placeholder}
+          required
+        />
+      </div>
+    )
+  );
+};
+
+export default Input;
