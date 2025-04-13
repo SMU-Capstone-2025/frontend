75677f762204d863925f4ab7355ccdec67f0af3d
jaeyoungmun <mjy0694@naver.com>
2025-04-13 17:25:59 +0900

    FEAT: 클라우드 배포 url적용

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const axiosInstanceWithHeader = axios.create({
-  baseURL: "http://172.17.64.116:8080",
+  baseURL: "http://35.202.85.190:8080/",
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJjYXRlZ29yeSI6ImFjY2VzcyIsImVtYWlsIjoidGVzdDFAZ21haWwuY29tIiwicm9sZSI6IlJPTEVfTUFOQUdFUiIsImlhdCI6MTc0NDE4MTI2MiwiZXhwIjoxNzQ0MTgzMDYyfQ.nNBt4xxN9bI_OqN-Z-IxAF1KY_GFkUMmKhhvV9yKS8M`,
+    Authorization: `Bearer `,
   },
 });
 
 const axiosInstanceNoHeader = axios.create({
-  baseURL: "http://172.17.64.116:8080",
+  baseURL: "http://35.202.85.190:8080/",
 });
 export { axiosInstanceNoHeader, axiosInstanceWithHeader };
