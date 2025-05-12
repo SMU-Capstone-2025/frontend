18cfaf9b7fd74e72267a683e0e8c04adb863fb95
jaeyoungmun <mjy0694@naver.com>
2025-05-12 16:03:17 +0900

    FEAT(mypage): 사용자 정보 카드 컴포넌트 생성

---
+++ src/components/UserIdCard/UserIdCard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const UserIdCard = () => {
+  return (
+    <div className="w-60 h-[100] p-3.5 bg-white rounded-xl outline outline-1 outline-offset-[-1px] outline-gray-200 flex flex-col justify-start items-center gap-2.5 overflow-hidden">
+      <div className=" w-full flex justify-start items-center gap-2">
+        <img
+          className="w-12 h-12 rounded-full"
+          src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-978409_1280.png"
+          alt="profile"
+        />
+        <div className="flex flex-col justify-start items-start gap-1">
+          <div className="flex-row text-gray-900 text-base font-bold font-['Pretendard']">
+            이름
+          </div>
+          <div className="flex-row text-gray-500 text-sm font-normal font-['Pretendard']">
+            이메일
+          </div>
+        </div>
+      </div>
+      <div
+        data-property-1="basics"
+        className="self-stretch h-7 px-12 py-[5px] bg-gray-600 rounded inline-flex justify-center items-center gap-2.5"
+      >
+        <div className="justify-start text-white text-xs font-bold font-['Pretendard'] leading-none [text-shadow:_0px_4px_20px_rgb(0_0_0_/_0.15)]">
+          기본 요금제
+        </div>
+      </div>
+      <div className="text-center justify-center text-gray-400 text-xs font-bold font-['Pretendard'] leading-none">
+        사용자 정보 수정
+      </div>
+    </div>
+  );
+};
+
+export default UserIdCard;
