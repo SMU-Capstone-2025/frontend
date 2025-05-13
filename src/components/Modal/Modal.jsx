97ba750f03bf5ae03d37f4e0ccb51afa9a619247
SonYoungJun <dudwnsths1@naver.com>
2025-05-14 01:54:49 +0900

    refactor(modal): 모달 버튼 크기 조정

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,8 +4,9 @@ import CloseOn from "../../assets/icons/Close/CloseOn";
 import Bell from "../../assets/icons/Bell/BellOn";
 import Caesarzkn from "../../assets/icons/Caesarzkn/Caesarzkn";
 
-const Modal = ({ isOpen, onClose, children, onDelete, showDelete }) => {
+const Modal = ({ isOpen, onClose, onDelete, showDelete, children }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   if (!isOpen) return null;
 
   const toggleDropdown = () => setIsDropdownOpen((prev) => !prev);
@@ -24,34 +25,29 @@ const Modal = ({ isOpen, onClose, children, onDelete, showDelete }) => {
         className="bg-white w-full max-w-[1042px] h-full max-h-[768px] p-[30px] flex flex-col items-end gap-5 rounded-xl border border-[#E5E7EB] relative animate-fadeIn overflow-auto"
         onClick={(e) => e.stopPropagation()}
       >
-        {/* 버튼 영역 */}
         <div className="flex items-start gap-5">
           {showDelete && (
             <div className="relative">
-              {/* 설정 버튼 */}
               <button
-                onClick={() => setIsDropdownOpen((prev) => !prev)}
+                onClick={toggleDropdown}
                 className="hover:opacity-40 fill-[#D9D9D9]"
               >
                 <EllypsisOn />
               </button>
               {isDropdownOpen && (
-                <div className="absolute top-full right-0 flex flex-col items-start gap-3 px-4 py-[14px] rounded-lg border border-[#D2D5DA] bg-white z-20 w-[122px]">
-                  {/* 삭제하기 버튼 */}
+                <div className="absolute top-full right-0 flex flex-col items-start gap-2 px-4 py-[14px] rounded-lg border border-[#D2D5DA] bg-white z-20 w-[140px]">
                   <button
                     onClick={handleDelete}
-                    className="flex items-center gap-2 max-w-[90px] w-full rounded-md hover:w-[106px] hover:h-[36px] hover:rounded-[6px] hover:bg-[#D9D9D9] hover:opacity-40"
+                    className="flex items-center gap-2 w-full rounded-md hover:bg-[#D9D9D9] hover:opacity-40 p-1"
                   >
                     <Caesarzkn />
-                    <span className="text-[#E40505] font-pretendard text-sm font-semibold leading-[140%] tracking-[-0.14px]">
+                    <span className="text-[#E40505] font-pretendard text-sm font-semibold">
                       삭제하기
                     </span>
                   </button>
-
-                  {/* 알림받기 버튼 */}
-                  <button className="flex items-center gap-2 max-w-[90px] w-full rounded-md hover:w-[106px] hover:h-[36px] hover:rounded-[6px] hover:bg-[#D9D9D9] hover:opacity-40">
+                  <button className="flex items-center gap-2 w-full rounded-md hover:bg-[#D9D9D9] hover:opacity-40 p-1">
                     <Bell />
-                    <span className="text-[#374151] font-pretendard text-sm font-semibold leading-[140%] tracking-[-0.14px]">
+                    <span className="text-[#374151] font-pretendard text-sm font-semibold">
                       알림받기
                     </span>
                   </button>
@@ -59,7 +55,10 @@ const Modal = ({ isOpen, onClose, children, onDelete, showDelete }) => {
               )}
             </div>
           )}
-          <button onClick={onClose} className="hover:opacity-40 fill-[#D9D9D9]">
+          <button
+            onClick={onClose}
+            className="hover:opacity-40 fill-[#D9D9D9] w-6 h-6"
+          >
             <CloseOn />
           </button>
         </div>
