a7abcc2afce58d0118d8dd6f2fbf7f2bafb6c49d
SonYoungJun <dudwnsths1@naver.com>
2025-07-31 23:06:51 +0900

    refactor(router): 프로젝트 라우트 구조를 section/projectId 형태로 리팩토링

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,18 +1,13 @@
-import { Navigate } from "react-router-dom";
 import Project from "../pages/wireframe-project/Project";
 import VideoRoom from "../pages/wireframe-videoroom/VideoRoom";
 import RootLayout from "../layout/root-layout";
 import DoctalkMain from "../pages/doctalkMain/DoctalkMain";
-import DocumentDetailPage from "../pages/wireframe-project/document-page/DocumentDetailPage";
-import Settings from "../pages/wireframe-project/settings-page/Settings";
-import WorkBoard from "../pages/wireframe-project/workboard-page/WorkBoard";
-import Document from "../pages/wireframe-project/document-page/Document";
 import Login from "../pages/login/Login";
 import Signup from "../pages/Signup/Signup";
 import Mypage from "../pages/Mypage/Mypage";
-import DocumentMainPage from "../pages/wireframe-project/document-page/DocumentMainPage";
 import DocumentCreatePage from "../pages/wireframe-project/document-page/DocumentCreatePage";
 import PasswordReset from "../pages/PasswordReset/PasswordReset";
+import ProjectRouter from "../pages/wireframe-project/ProjectRouter";
 
 const Router = [
   {
@@ -24,26 +19,21 @@ const Router = [
       { path: "signup", element: <Signup /> },
       { path: "mypage", element: <Mypage /> },
       { path: "password-reset", element: <PasswordReset /> },
+
       {
-        path: "project",
+        path: "project/:section/:projectId",
         element: <Project />,
         children: [
-          { index: true, element: <Navigate to="workboard" /> },
-          { path: "workboard", element: <WorkBoard /> },
           {
-            path: "document",
-            element: <Document />,
-            children: [
-              { index: true, element: <DocumentMainPage /> },
-              { path: ":id", element: <DocumentDetailPage /> },
-            ],
+            path: "",
+            element: <ProjectRouter />,
           },
-
-          { path: "settings", element: <Settings /> },
         ],
       },
-      { path: "video", element: <VideoRoom /> },
+
       { path: "project/document/new", element: <DocumentCreatePage /> },
+
+      { path: "video", element: <VideoRoom /> },
     ],
   },
 ];
