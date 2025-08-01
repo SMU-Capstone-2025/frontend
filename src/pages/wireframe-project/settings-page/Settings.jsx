f025f871dd297a3bb01787555285c97c5ca3eb32
SonYoungJun <dudwnsths1@naver.com>
2025-08-01 19:17:59 +0900

    feat(setting): 더미 데이터 제거 및 api 연동
    - 기존에 존재하는 더미 데이터 제거
    - 백엔드와의 연동을 통해 실제 데이터로 받음
    - 프로젝트 정보 저장버튼 생성

--- src/pages/wireframe-project/settings-page/Settings.jsx
+++ src/pages/wireframe-project/settings-page/Settings.jsx
@@ -1,23 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./Settings.styled";
 import ProjectInfo from "../../../components/settings-element/project-settings/ProjectInfo";
 import MemberManager from "../../../components/settings-element/member-management/MemberManager";
-
-const dummyUser = [
-  {
-    id: 1,
-    name: "사용자",
-    email: "username1@gmail.com",
-    role: "owner",
-  },
-];
+import { fetchProject, updateProject } from "../../../api/projectApi";
+import { useOutletContext } from "react-router-dom";
 
 const Settings = () => {
+  const { projectId } = useOutletContext();
   const [projectName, setProjectName] = useState("");
   const [coverImage, setCoverImage] = useState(null);
   const [projectDescription, setProjectDescription] = useState("");
-  const [members, setMembers] = useState(dummyUser);
+  const [members, setMembers] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetchProject(projectId);
+        const { name, description, coworkers } = res.result;
+
+        setProjectName(name);
+        setProjectDescription(description);
+        setCoverImage(null); // 커버 URL이 있다면 넣고, 없으면 유지
+
+        setMembers(
+          coworkers.map((user, idx) => ({
+            id: idx + 1,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          }))
+        );
+      } catch (err) {
+        console.error("프로젝트 정보 불러오기 실패", err);
+      }
+    };
+
+    fetchData();
+  }, [projectId]);
+
+  const handleUpdateProject = async () => {
+    try {
+      await updateProject(projectId, projectName, projectDescription);
+      alert("✅ 프로젝트 정보가 저장되었습니다.");
+    } catch (err) {
+      alert("❌ 프로젝트 정보 저장 실패");
+      console.error(err);
+    }
+  };
   return (
     <S.Container>
       <ProjectInfo
@@ -27,8 +56,13 @@ const Settings = () => {
         setCoverImage={setCoverImage}
         projectDescription={projectDescription}
         setProjectDescription={setProjectDescription}
+        onSave={handleUpdateProject}
       />
-      <MemberManager members={members} setMembers={setMembers} />
+      <MemberManager
+        projectId={projectId}
+        members={members}
+        setMembers={setMembers}
+      />
     </S.Container>
   );
 };
